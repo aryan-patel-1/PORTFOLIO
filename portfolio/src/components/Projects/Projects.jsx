12362d36ec0dd0c591aa47c53a7a17174c39111a
aryan <aryanpatel150304@gmail.com>
2025-07-17 18:02:48 +0200

    Ajout des projets

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -3,20 +3,32 @@ import "./Projects.scss";
 export default function Projects() {
   const projects = [
     {
-      title: "ArgentBank",
+      title: "ArgentBank - Application bancaire",
       description:
         "Application web de gestion de comptes bancaires. Implémentation des composants interactifs et de l'authentification.",
       tech: "React · Sass · Redux Toolkit",
-      link: "#", 
-    },
-    {
-      title: "Portfolio personnel",
-      description:
-        "Site vitrine one-page responsive pour présenter mes compétences et projets. ",
-      tech: "React · Vite · Sass",
       link: "#",
     },
-    
+    {
+      title: "Nina Carducci - Optimisation SEO",
+      description:
+        "Optimisation des performances et du référencement du site de Nina Carducci. Compression des images, ajout de balises meta, intégration Schema.org pour le référencement local et amélioration de l’accessibilité.",
+      tech: "SEO · Lighthouse · Schema.org · Accessibilité Web",
+      link: "https://aryan-patel-1.github.io/nina-carducci-p8/",
+    },
+    {
+      title: "Booki – Plateforme de réservation d'hébergements et d'activités",
+      description: "Intégration d'une maquette Figma en HTML et CSS pour une application responsive, permettant aux utilisateurs de trouver des hébergements et des activités dans la ville de leur choix. Développement desktop-first avec animations CSS.",
+      tech: "HTML5 · CSS3 · Flexbox · Animations CSS · Responsive Design ",
+      link: "https://aryan-patel-1.github.io/BOOKI-P3/",
+    },
+    {
+      title: "Ohmyfood – Plateforme  de menus gastronomiques",
+      description: "Développement d’un site responsive mobile first pour 4 restaurants, permettant de composer un menu personnalisé à l’avance. Animations CSS, structure SASS, et intégration fidèle des maquettes Figma.",
+      tech: "HTML · CSS3/SASS · Responsive design ",
+      link: "https://aryan-patel-1.github.io/OHMYFOOD/",
+    },
+
   ];
 
   return (
